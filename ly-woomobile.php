93f440e9ce0bf5b699de360c88aa40053cf4a45d
lyabs243 <loicyabili@outlook.com>
2020-08-13 20:25:11 +0200

    Init extension plugin header

---
+++ ly-woomobile.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name: Woo Mobile
+ * Description: This plugin creates a REST API which will allow communicatio with a mobile application to transform a website into a mobile application.
+ * Version: 1.0
+ * Author: Loic Yabili, Lyabs Media
+ * License: GPLV3
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.fr.html
+ *
+ * Woo Mobile is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * Woo Mobile is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Woo Mobile. If not, see https://www.gnu.org/licenses/gpl-3.0.fr.html.
+ */
